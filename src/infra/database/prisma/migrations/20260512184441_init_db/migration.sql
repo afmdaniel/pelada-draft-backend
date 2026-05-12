8fdf3b5ede9b76c18bf6d13f6bc02c9d21302b25
Daniel Melo <danielfarias380@gmail.com>
2026-05-12 15:45:30 -0300

    chore: setup prisma schema and initial migration

---
+++ src/infra/database/prisma/migrations/20260512184441_init_db/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Player" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "stars" INTEGER NOT NULL,
+    "position" TEXT NOT NULL,
+
+    CONSTRAINT "Player_pkey" PRIMARY KEY ("id")
+);
