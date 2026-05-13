60c9fd4f82c7f77de54e87dc377f42bf8d850653
Daniel Melo <danielfarias380@gmail.com>
2026-05-12 22:38:28 -0300

    feat: implement add player use case

---
+++ src/core/use-cases/aadd-player-to-pelada.ts
@@ -0,0 +1,31 @@
+import { Player } from '../entities/player.entity';
+import { PeladaRepository } from '../repositories/pelada-repository';
+
+interface AddPlayerInput {
+  name: string;
+  stars: number;
+  position?: 'Zaga' | 'Meio' | 'Ataque' | 'Geral';
+  peladaId: string;
+}
+
+export class AddPlayerToPelada {
+  constructor(private peladaRepository: PeladaRepository) {}
+
+  async execute(input: AddPlayerInput): Promise<Player> {
+    const pelada = await this.peladaRepository.findById(input.peladaId);
+    if (!pelada) {
+      throw new Error('Pelada não encontrada.');
+    }
+
+    const player = new Player({
+      name: input.name,
+      stars: input.stars,
+      position: input.position,
+      peladaId: input.peladaId,
+    });
+
+    await this.peladaRepository.addPlayer(player);
+
+    return player;
+  }
+}
