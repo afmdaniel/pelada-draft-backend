a483610827a1bf1169ef67f187a1d6329ca4fb59
Daniel Melo <danielfarias380@gmail.com>
2026-05-13 12:39:13 -0300

    chore: centralize PeladaRepository as Aggregate Root

--- src/core/repositories/player-repository.ts
+++
@@ -1,8 +0,0 @@
-import { Player } from '../entities/player.entity';
-
-export abstract class PlayerRepository {
-  abstract create(player: Player): Promise<void>;
-  abstract findManyByGroup(groupId: string): Promise<Player[]>;
-  abstract update(player: Player): Promise<void>;
-  abstract delete(playerId: string): Promise<void>;
-}
