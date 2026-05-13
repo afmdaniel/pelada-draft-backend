2a85562a3ead9cdd1f56b9e6c4c2b6c9a2cbd679
Daniel Melo <danielfarias380@gmail.com>
2026-05-13 11:04:02 -0300

    chore: move http.module to root http directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { HttpModule } from './infra/http/http/http.module';
+import { HttpModule } from './infra/http/http.module';
 import { DatabaseModule } from './infra/database/database.module';
 import { ConfigModule } from '@nestjs/config';
 
---
+++ src/infra/http/http.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { PlayerController } from './controllers/player/player.controller';
+import { AddPlayerToPelada } from '../../core/use-cases/add-player-to-pelada';
+import { DatabaseModule } from '../database/database.module';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [PlayerController],
+  providers: [AddPlayerToPelada],
+})
+export class HttpModule {}
