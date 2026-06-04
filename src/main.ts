f62f1e68f1ac0b7c73da79e40605498491d11eed
Daniel Melo <danielfarias380@gmail.com>
2026-06-04 14:22:27 -0300

    chore(infra): enable trust proxy for secure HTTPS handling

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { GlobalExceptionFilter } from './infra/http/filters/global-exception.filter';
 import { TransformInterceptor } from './infra/http/interceptors/transform.interceptor';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy', Number(process.env.TRUST_PROXY ?? 0));
 
   app.enableCors({
     origin: process.env.FRONTEND_URL,
