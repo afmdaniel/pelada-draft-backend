660994ee9ebc2a530f231703ba667285e71b07ee
Daniel Melo <danielfarias380@gmail.com>
2026-06-04 13:19:15 -0300

    feat(http): add global filters and interceptors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { GlobalExceptionFilter } from './infra/http/filters/global-exception.filter';
+import { TransformInterceptor } from './infra/http/interceptors/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const reflector = app.get(Reflector);
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
+
+  app.useGlobalInterceptors(new TransformInterceptor(reflector));
 
   app.useGlobalPipes(
     new ValidationPipe({
